41e57297aee7d8771fea1d3f50064b22bc84f9d1
sboukiou <sboukiou@student.1337.ma>
2024-12-28 10:29:51 +0100

    testing get_next_line first prottype

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,49 @@
 #include <string.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 1024
 #endif
+
+char	*_realloc(char *line, int size)
+{
+	char	*new_line;
+	int		i;
+	new_line = (char *)malloc(sizeof(char) * size);
+	if (!new_line)
+		return (NULL);
+	i = 0;
+	while (line[i])
+	{
+		new_line[i] = line[i];
+		i++;
+	}
+	free(line);
+	return (new_line);
+}
 
 char	*get_next_line(int fd)
 {
+	char	*line;
+	int		index;
+	int		count;
 
+	if (fd < 0)
+		return (NULL);
+	index = 0;
+	count = BUFFER_SIZE;
+	line = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	if (!line)
+		return (NULL);
+	while (read(fd, line + index, 1) > 0)
+	{
+		if (line[index] == '\n')
+			return (line);
+		if (index >= count)
+		{
+			count *= 2;
+			line = realloc(line, count);
+		}
+		index++;
+	}
+	return (line);
 }
