02045aea7411e6a6f125628f2346eee4d1fcbce5
sboukiou <sboukiou@student.1337.ma>
2024-12-28 10:32:54 +0100

    no utils so far

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,20 +1,3 @@
 #include "get_next_line.c"
 
 
-void	*ft_calloc(size_t nememb, size_t size)
-{
-	void	*block;
-	int	i;
-
-	if (!nmemb || !size)
-		return (NULL);
-	if ((nmemb * size) / size != nmemb)
-		return (NULL);
-	block = malloc(nmemb * size);
-	while (i < nmemb * size)
-	{
-		(char *)block[i] = '\0';
-		i++;
-	}
-	return (block);
-}
