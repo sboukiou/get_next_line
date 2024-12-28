ca81ce3dc351c227cc92e667b77f3d602ce4bdd1
sboukiou <sboukiou@student.1337.ma>
2024-12-28 08:23:40 +0100

    Bak on da trak

---
+++ get_next_line.c
@@ -0,0 +1,12 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <string.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+char	*get_next_line(int fd)
+{
+
+}
---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 24
+#endif
+
+char *get_next_line(int fd);
+#endif
