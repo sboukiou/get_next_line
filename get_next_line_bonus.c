947d17aa861e0701449bc974d484a42e23a55b0a
sboukiou <sboukiou@student.1337.ma>
2025-01-03 04:13:03 +0100

    addin bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,57 @@
+#include "get_next_line.h"
+#include <stdio.h>
+
+char	*get_full_buffer(char *buffer, int	fd)
+{
+	int		lines_read;
+
+	while (ft_strchr(buffer, NEWL) == NULL)
+	{
+		buffer = ft_realloc(buffer, (ft_strlen(buffer) + BUFFER_SIZE + 1) * sizeof(char));
+		if (!buffer)
+			return (NULL);
+
+		lines_read = read(fd, buffer + ft_strlen(buffer), BUFFER_SIZE);
+		if (lines_read < 1)
+			return (buffer);
+	}
+	return (buffer);
+}
+
+char    *reset_buffer(char *buffer)
+{
+        char    *temp;
+        char    *new_buffer;
+
+		new_buffer = NULL;
+        if (!ft_strlen(buffer))
+                return (buffer);
+        temp = ft_strchr(buffer, '\n');
+		new_buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+        if (temp && temp[1] != '\0')
+                ft_strlcpy(new_buffer, temp + 1, ft_strlen(temp + 1) + 1);
+        free(buffer);
+        buffer = NULL;
+        return (new_buffer);
+}
+
+
+
+char	*get_next_line(int file_d)
+{
+	char		*line;
+	static char	*buffer;
+
+	if (file_d < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	line = NULL;
+	buffer = get_full_buffer(buffer, file_d);
+	if (!buffer)
+		return (NULL);
+	line = extend_line(line, buffer);
+	buffer = reset_buffer(buffer);
+
+	if (!ft_strlen(buffer))
+		free(buffer), buffer =  NULL;
+	return (line);
+}
