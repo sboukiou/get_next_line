19045950572ffc9f2e2b609e1161c0a3dc02f06d
sboukiou <sboukiou@student.1337.ma>
2025-01-07 05:05:35 +0100

    fixing readin from empty line multiple times

--- get_next_line.c
+++ get_next_line.c
@@ -13,10 +13,7 @@ char	*fill_buffer(char *buffer, int fd)
 		if (!buffer)
 			return (NULL);
 		lines_read = read(fd, buffer + buffer_len, BUFFER_SIZE);
-		if (lines_read >= 0)
-			buffer[buffer_len + lines_read] = '\0';
-		else
-			buffer[buffer_len] = '\0';
+		buffer[lines_read + buffer_len] = '\0';
 		if (lines_read < 1)
 			return (buffer);
 		buffer_len += lines_read;
@@ -30,11 +27,11 @@ char	*extract_line(char *buffer)
 	int	line_len;
 
 	if (!buffer || !ft_strlen(buffer, '\0'))
-		return (free(buffer), NULL);
+		return (NULL);
 	line_len = ft_strlen(buffer, '\n');
 	line = (char *)malloc((size_t)(sizeof(char) * (line_len + 1)));
 	if (!line)
-		return (free(buffer), NULL);
+		return (NULL);
 	ft_strlcpy(line, buffer, line_len + 1);
 	return (line);
 }
@@ -51,12 +48,13 @@ char	*reset_buffer(char *buffer)
 		temp_len = ft_strlen(temp + 1, '\0');
 		new_buffer = (char *)malloc((size_t)(sizeof(char) * (temp_len + 1)));
 		if (!new_buffer)
-			return (free(buffer), NULL);
+			return (free(buffer), buffer = NULL, NULL);
 		ft_strlcpy(new_buffer, temp + 1, temp_len + 1);
-		free(buffer);
+		free(buffer), buffer = NULL;
 		return (new_buffer);
 	}
 	free(buffer);
+	buffer = NULL;
 	return (NULL);
 }
 
@@ -65,8 +63,14 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	buffer = fill_buffer(buffer, fd);
+	if (!buffer  || !ft_strlen(buffer, '\0'))
+		return (free(buffer), buffer = NULL, NULL);
 	line = extract_line(buffer);
+	if (!line)
+		return (free(buffer), buffer = NULL, NULL);
 	buffer = reset_buffer(buffer);
 	return (line);
 }
